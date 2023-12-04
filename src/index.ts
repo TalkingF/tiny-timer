669d4b95b7ab1b1c956d20abfa98fefcae231928
TalkingF <talkingfinger05@gmail.com>
2023-12-04 22:25:37 +1030

    feat: initial time format

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ function setTimer(count: number, time: number) {
   let time_element = document?.getElementById('timer');
   if (count > time) return;
   if (time_element != null) {
-    time_element.innerText = time.toString();
+    time_element.innerText = Math.floor(time / 60) + ":" + time % 60;
     setTimeout(() => setTimer(count, time - 1), 1000);
   }
 }
+
