0cf979c41481e075e4ea2be2efc9daef78438567
TalkingF <talkingfinger05@gmail.com>
2023-12-05 17:28:55 +1030

    feat: update timer to display and minutes and css

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 function setTimer(count: number, time: number) {
   console.log('executed');
-  let time_element = document?.getElementById('timer');
+  let time_element = document?.getElementById('time-text');
   if (count > time) return;
   if (time_element != null) {
     time_element.innerText = Math.floor(time / 60).toLocaleString('en-Us' , {minimumIntegerDigits: 2})
