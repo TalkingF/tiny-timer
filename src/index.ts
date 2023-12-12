8dbba10754acfc1308d4020bd6eae34e2a76cf88
TalkingF <talkingfinger05@gmail.com>
2023-12-12 19:28:19 +1030

    refactor: change pause to resume where left off

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,25 @@
 let pause = false;
-let pause_time = 0;
-this.addEventListener('keypress', event => {
-  if (event.key === ' ' || event.key === 'Enter') {
-    if (!pause && pause_time != 0) setTimer(pause_time);
-    pause = !pause;
-  }
-})
+let time = 0;
+function changePauseState() {
+  if (pause) {
+    pause = false;
+    setTimer(time);
+  } 
+  else pause = true;
+  
+}
 
-async function setTimer(time: number) {
+async function setTimer(time_length: number) {
+  time = time_length;
   let time_element = document?.getElementById('time-text');
   if (time_element != null) {
-    let i = 0;
-    while (i < time) {
-      if (pause) {
-        pause_time = time - i;
-        return;
-      }
-      time_element.innerText = Math.floor((time - i) / 60)
+    while (time > 0 && !pause) {
+      time_element.innerText = Math.floor((time) / 60)
       .toLocaleString('en-Us' , {minimumIntegerDigits: 2})
-      + ':' + ((time - i) % 60).toLocaleString('en-Us' , {minimumIntegerDigits: 2});
+      + ':' + ((time) % 60).toLocaleString('en-Us' , {minimumIntegerDigits: 2});
       const timer = new Promise(res => setTimeout(res, 1000));
       await timer;
-      i++;
+      time--;
     }
   }
 }
